af4364f2d4b633f2d70b12801f00d7feb70bd413
Jonathan <poisson.jonathan777@gmail.com>
2016-08-20 18:33:08 -0400

    Updated ports to reflect what works at Summit

--- assets/app/services/chat.service.ts
+++ assets/app/services/chat.service.ts
@@ -3,7 +3,7 @@ import { Observable } from 'rxjs/Observable';
 import * as io from 'socket.io-client';
 
 export class ChatService {
-  private url = 'http://localhost:3000';
+  private url = 'scfldevjp.summit.local';
   private socket;
 
   sendMessage(message, username) {
--- gulpfile.js
+++ gulpfile.js
@@ -80,7 +80,7 @@ gulp.task('browser-sync', ['nodemon'], function() {
   // for more browser-sync config options: http://www.browsersync.io/docs/options/
   browserSync({
     // informs browser-sync to proxy our expressjs app which would run at the following location
-    proxy: 'http://localhost:3000',
+    proxy: 'scfldevjp.summit.local:3000',
     port: 4000,
   });
 });
